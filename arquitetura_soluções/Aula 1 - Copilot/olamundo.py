d5ecc60861053de943e4b68677a6137b78dea4cb
wpceid <wpceid@gmail.com>
2025-02-24 19:45:03 -0300

    Atualizações de pasta

--- arquitetura_soluções/Aula 1 - Copilot/olamundo.py
+++ arquitetura_soluções/Aula 1 - Copilot/olamundo.py
@@ -2,7 +2,7 @@
 # Solicita ao usuário para inserir o primeiro número inteiro
 numero1 = int(input("Digite o primeiro número inteiro: "))
 
-# Solicita ao usuário para inserir o segundo número inteiro
+# Solicita ao usuário para inserir o segundo núm8ero inteiro
 numero2 = int(input("Digite o segundo número inteiro: "))
 
 # Calcula a soma dos dois números
