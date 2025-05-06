b52061ec7dfd510127fff681f403cab35f46c6d6
wpceid <wpceid@gmail.com>
2025-05-06 20:59:31 -0300

    Estudando javascript

---
+++ fundamentos_programacao_web/aula_9_js/pratica/JS/altera_imagem.js
@@ -0,0 +1,11 @@
+document.getElementById("btn_imagem_triste").onclick = function() {
+    document.getElementById("ponto_interrogacao").src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhjE-kjrxjXEQbrU20Lh4BsTrHX1UH59Vwdg&s'
+}
+
+document.getElementById('btn_imagem_feliz').onclick = function() {
+    document.getElementById('ponto_interrogacao').src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdz8E2PfJJLnlL_X-ZqMmsNJUaHBD9eMvXyw&s'
+}
+
+document.getElementById('altera_texto').onclick{
+    document.getElementById('texto').innerHTML = "Mudamos o texto kkkk"
+}
