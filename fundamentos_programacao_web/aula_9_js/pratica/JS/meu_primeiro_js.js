a7c30aac7c3ee6de2e9457f73f2763fb97e4ba59
wpceid <wpceid@gmail.com>
2025-05-12 18:52:22 -0300

    js classes

--- fundamentos_programacao_web/aula_9_js/pratica/JS/meu_primeiro_js.js
+++ fundamentos_programacao_web/aula_9_js/pratica/JS/meu_primeiro_js.js
@@ -6,6 +6,8 @@ document.getElementById('btn_imagem_feliz').onclick = function() {
     document.getElementById('ponto_interrogacao').src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdz8E2PfJJLnlL_X-ZqMmsNJUaHBD9eMvXyw&s'
 }
 
-document.getElementById('altera_texto').onclick{
+document.getElementById('altera_texto').onclick = 
+function ()
+{
     document.getElementById('texto').innerHTML = "Mudamos o texto kkkk"
 }
